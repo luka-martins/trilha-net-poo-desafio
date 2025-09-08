c53e421f9f16c5a640329ae53e4552580520d91b
Lucas Alexandre <lucas.amp98@gmail.com>
2025-09-08 09:53:46 -0300

    implemtação dos desafios propostos

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,14 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
-    {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+    public class Nokia : Smartphone {
+        public Nokia(string numero, string modelo, string IMEI, int memoria) : base(numero, modelo, IMEI, memoria)
+        {
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine("Instalando " + nomeApp);
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,23 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Criando um Nokia
+        Smartphone nokia = new Nokia("99999-1111", "Nokia Tijolão", "123456789012345", 64);
+        Console.WriteLine("=== Teste com Nokia ===");
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine();
+
+        // Criando um iPhone
+        Smartphone iphone = new Iphone("88888-2222", "iPhone 15", "987654321098765", 128);
+        Console.WriteLine("=== Teste com iPhone ===");
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
